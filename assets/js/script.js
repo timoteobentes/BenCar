4634f22254387f09448769e3799476418a662f68
Timóteo Freires de Souza Bentes <bentestimoteo@gmail.com>
2022-10-21 16:50:31 -0400

    teste5

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,17 +27,80 @@ function hide() {
     $(".deletar").hide();
 }
 
+// Consumo da API
+
+// GET
+fetch("https://api-bencar.herokuapp.com/consulta", {
+    headers: {
+        "Accept": "application/json"
+    }
+})
+.then(res => res.json())
+.then(json => {
+    var linha = `<thead>
+                    <th>ID</th>
+                    <th>Motorista</th>
+                    <th>CPF</th>
+                    <th>Telefone</th>
+                    <th>Placa</th>
+                    <th>Ações</th>
+                </thead>`
+    json.forEach((data) => {
+        linha +=    `<tr>
+                        <td>${data._id}</td>
+                        <td>${data.nome}</td>
+                        <td>${data.cpf}</td>
+                        <td>${data.telefone}</td>
+                        <td>${data.placaCarro}</td>
+                        <td>
+                            <button class="btn-editar" onclick="Editar()"><ion-icon name="pencil"></ion-icon></button>
+                            <button class="btn-deletar" onclick="Deletar()"><ion-icon name="trash"></ion-icon></button>
+                        </td>
+                    </tr>`
+    })
+
+    document.getElementById("table").innerHTML = linha
+})
+
+
+
+
+// POST
+
 const cadastro = document.getElementById("cadastro");
 cadastro.addEventListener("submit", (e) => {
     e.preventDefault();
-    const cad = new FormData(cadastro);
-    fetch("localhost:3000/cliente", {
+
+    const nome = document.getElementById("nome").value;
+    const cpf = document.getElementById("cpf").value;
+    const telefone = document.getElementById("telefone").value;
+    const placaCarro = document.getElementById("placaCarro").value;
+
+    const cad = {
+        "nome": nome,
+        "cpf": cpf,
+        "telefone": telefone,
+        "placaCarro": placaCarro
+    }
+
+    console.log(cad)
+
+    //const cad = JSON.parse(cadastro);
+    fetch("https://api-bencar.herokuapp.com/cadastro", {
         method: "POST",
-        body: cad
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(cad)
     })
     .then(res => {
-        if(res.status == 200) {
+        if(res.status == 201) {
             alert("Cadastrado com sucesso");
+            hide();
         }
     })
 })
+
+
+
+// PUT
